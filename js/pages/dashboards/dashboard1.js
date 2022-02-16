d14039fe11997c09e81214544cdb5fb292901916
kido-fullstack <ziauddin.sayyed@kido.school>
2022-02-16 11:57:34 +0530

    Changes

--- js/pages/dashboards/dashboard1.js
+++ js/pages/dashboards/dashboard1.js
@@ -13,40 +13,74 @@ $(function () {
     // Newsletter
     // ============================================================== 
         
-        var goBackDays = 7;
+    //     for(var i = 0; i < goBackDays; i++) {
+    //     var newDate = new Date(today.setDate(today.getDate() - 1));
+    //     let monthIndex = newDate.getMonth();
+    //     let monthName = monthNames[monthIndex];
+    //     daysSorted.push(newDate.getDate()+" "+monthName);
+    //     }
 
+    //     //ct-visits
+    //     new Chartist.Line('#ct-visits', {
+    //     labels: daysSorted,
+    //     series: [
+    //         [4, 2, 4, 2, 4, 3, 4, 2]
+    //     ]
+    // }, {
+    //     top: 0,
+    //     low: 1,
+    //     showPoint: true,
+    //     fullWidth: true,
+    //     plugins: [
+    //         Chartist.plugins.tooltip()
+    //     ],
+    //     axisY: {
+    //         labelInterpolationFnc: function (value) {
+    //             return (value) + 'cnt';
+    //         }
+    //     },
+    //     showArea: true
+    // });
+
+    make_chart("ct-visits");
+    make_chart("ct-sm-visits");
+
+    function make_chart(div_id) {
+
+        var goBackDays = 7;
         var today = new Date();
         var daysSorted = [];
 
         for(var i = 0; i < goBackDays; i++) {
-        var newDate = new Date(today.setDate(today.getDate() - 1));
-        let monthIndex = newDate.getMonth();
-        let monthName = monthNames[monthIndex];
-        daysSorted.push(newDate.getDate()+" "+monthName);
-        }
-
-        //ct-visits
-        new Chartist.Line('#ct-visits', {
-        labels: daysSorted,
-        series: [
-            [4, 2, 4, 2, 4, 3, 4, 2]
-        ]
-    }, {
-        top: 0,
-        low: 1,
-        showPoint: true,
-        fullWidth: true,
-        plugins: [
-            Chartist.plugins.tooltip()
-        ],
-        axisY: {
-            labelInterpolationFnc: function (value) {
-                return (value) + 'cnt';
+            var newDate = new Date(today.setDate(today.getDate() - 1));
+            let monthIndex = newDate.getMonth();
+            let monthName = monthNames[monthIndex];
+            daysSorted.push(newDate.getDate()+" "+monthName);
             }
-        },
-        showArea: true
-    });
+    
+            //ct-visits
+            new Chartist.Line('#'+div_id, {
+            labels: daysSorted,
+            series: [
+                [4, 2, 4, 2, 4, 3, 4, 2]
+            ]
+        }, {
+            top: 0,
+            low: 1,
+            showPoint: true,
+            fullWidth: true,
+            plugins: [
+                Chartist.plugins.tooltip()
+            ],
+            axisY: {
+                labelInterpolationFnc: function (value) {
+                    return (value) + 'cnt';
+                }
+            },
+            showArea: true
+        });
 
+    }
 
     var chart = [chart];
 
